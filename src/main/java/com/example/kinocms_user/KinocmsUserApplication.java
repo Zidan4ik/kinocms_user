1cba642a496c8d4158e1dab9e73207a7ea0904f8
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2024-08-23 13:27:35 +0300

    Initial commit

---
+++ src/main/java/com/example/kinocms_user/KinocmsUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.kinocms_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinocmsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KinocmsUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kinocms_user/KinocmsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinocms_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinocmsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
